611a81b968277463419f620236a9f915b919b3e4
sp200012 <107293323+sp200012@users.noreply.github.com>
2022-06-19 18:49:22 +0530

    Add files via upload

---
+++ Sumanthjava/src/com/Multiplication.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int a = 25;
+		int b = 5;
+		int c;
+		c=a*b;
+		System.out.println("result="+c);
+
+	}
+
+}
---
+++ Sumanthjava/src/com/Subtraction.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		int c;
+		c=a-b;
+		System.out.println("result="+c);
+
+	}
+
+}
